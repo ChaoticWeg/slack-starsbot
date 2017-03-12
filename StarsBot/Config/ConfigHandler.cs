8a05087bc78fd7b7bd9d75a9c23a131c01c4eb86
Shawn Lutch <shawn@chaoticweg.cc>
2017-03-12 18:02:03 -0500

    add configuration handling

---
+++ StarsBot/Config/ConfigHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+
+namespace StarsBot.Config
+{
+    public static class ConfigHandler
+    {
+        private static readonly Configuration ConfigManager =
+            ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+        private static readonly KeyValueConfigurationCollection Settings = ConfigManager.AppSettings.Settings;
+
+        public static int Slack_PostDelay => Convert.ToInt32(Settings["Slack_PostDelay"].Value);
+        public static bool Slack_Debugging => Convert.ToBoolean(Settings["Slack_Debugging"].Value);
+    }
+}
